26ee235cd4d667d6cdcc86b203a284b826b8a2ad
2867786874 <75127736+2867786874@users.noreply.github.com>
2023-11-13 16:33:40 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -34,17 +34,15 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler
     public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
-        //Duplicate entry 'zhangsan' for key 'employee.idx_username'
-        String message = ex.getMessage();
+        //Duplicate entry '尹飞跃' for key 'employee.idx_username'
+        String message=ex.getMessage();
         if(message.contains("Duplicate entry")){
             String[] split = message.split(" ");
             String username = split[2];
             String msg = username + MessageConstant.ALREADY_EXISTS;
             return Result.error(msg);
-        }else{
+        }else {
             return Result.error(MessageConstant.UNKNOWN_ERROR);
         }
     }
-
-
 }
